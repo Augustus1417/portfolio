fd0669d005360a2482c8bc5c80b01f8a03cf0a9f
Augustus1417 <jedxyril.cruz@gmail>
2024-12-18 09:03:47 +0800

    3D hover added to profile pic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let currentSlide = 0;
 const slides = document.querySelectorAll('.projects');
 const totalSlides = slides.length;
 const carouselContainer = document.querySelector('.carousel-container');
+const profilePic = document.getElementById('profile-pic');
 
 function updateSlides() {
     const slideWidth = slides[0].clientWidth; 
@@ -21,5 +22,23 @@ function moveSlide(direction) {
 
     updateSlides(); 
 }
+
+profilePic.addEventListener('mousemove', (e) => {
+  const rect = profilePic.getBoundingClientRect();
+  const x = e.clientX - rect.left; 
+  const y = e.clientY - rect.top;  
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = ((y - centerY) / centerY) * 50; 
+  const rotateY = ((x - centerX) / centerX) * -50; 
+
+  profilePic.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+profilePic.addEventListener('mouseleave', () => {
+  profilePic.style.transform = 'perspective(1000px) rotateX(0deg) rotateY(0deg)';
+});
 
 updateSlides();
